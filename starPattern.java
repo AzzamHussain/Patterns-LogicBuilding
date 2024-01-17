ead997abc35ac8d20600855674dce49037f51751
AzzamHussain <azzamhussain70@gmail.com>
2024-01-18 00:38:24 +0500

    Message

---
+++ starPattern.java
@@ -0,0 +1,18 @@
+  //* 
+  //**
+  //*** 
+  //**** 
+  //***** 
+public class starPattern {
+    public static void main(String[] args) {
+        for(int row=0;row<=4;row++){
+            for(int column=1;column<=row;column++){
+                System.out.print("*");
+            }
+            System.out.println();
+
+        }
+        
+    }
+    
+}
