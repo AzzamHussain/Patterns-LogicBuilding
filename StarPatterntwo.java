32efc4cd4f71364b65357a4d8592d79dc44a3d21
AzzamHussain <azzamhussain70@gmail.com>
2024-01-19 00:37:24 +0500

    AnotherPattern

--- StarPatterntwo.java
+++ StarPatterntwo.java
@@ -1,12 +1,15 @@
-  //* 
-  //**
-  //*** 
-  //**** 
+  
   //***** 
-public class starPattern {
+  //***
+  //** 
+  //* 
+
+  public class StarPatterntwo {
     public static void main(String[] args) {
         for(int row=1;row<=4;row++){
-            for(int column=1;column<=row;column++){
+
+            for(int column=4;column>=row;column--){
+
                 System.out.print("*");
             }
             System.out.println();
@@ -16,3 +19,4 @@ public class starPattern {
     }
     
 }
+
