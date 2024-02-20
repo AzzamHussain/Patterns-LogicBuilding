6d255864f6fa95d02ec216dbf1c3be41390c249b
AzzamHussain <azzamhussain70@gmail.com>
2024-02-20 23:35:40 +0500

    hollowSquare

---
+++ HollowSquare.java
@@ -0,0 +1,17 @@
+ class HollowSquarePattern {
+    public static void main(String[] args) {
+        int rows = 5;
+        
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= rows; j++) {
+                if (i == 1 || i == rows || j == 1 || j == rows) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
